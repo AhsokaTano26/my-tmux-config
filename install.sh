13103cfc9f4c10a2fc835e64a2885b9cc8b7e537
Tano <github@tano.asia>
2026-02-22 16:12:41 +0800

    Clean up install.sh and update user instructions
    Removed unnecessary comments and ensured the script provides user instructions.

--- install.sh
+++ install.sh
@@ -5,9 +5,7 @@ if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
     git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-# 2. 创建软链接 (将仓库里的文件链接到家目录)
-# 假设你在 ~/my-dotfiles 目录下执行
+# 2. 创建软链接
 ln -sf $(pwd)/tmux/tmux.conf ~/.tmux.conf
 
-# 3. 提示用户安装插件
 echo "Tmux 配置已同步。请进入 tmux 后按下: Ctrl+b + I (大写) 来安装插件。"
